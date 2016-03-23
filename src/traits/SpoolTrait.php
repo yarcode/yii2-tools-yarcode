49432cd697c0b55b8d433037b8a61159def0aaff
Valentin Konusov <rlng-krsk@yandex.ru>
2016-03-23 16:31:14 +0800

    SpoolTrait

---
+++ src/traits/SpoolTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace yarcode\base\traits;
+
+/**
+ * Class LoopTrait
+ * @package yarcode\base\traits
+ *
+ * Trait for easier making spool console commands
+ * All what you need:
+ * 1. Add trait to your console controller
+ * 2. Setup cron to run spool command
+ *  /var/www/{your_site_dir}/yii {your_controller-name}/spool {methodName} 1000 50
+ *
+ * where {methodName} like actionProcessOne
+ *
+ */
+trait LoopTrait
+{
+    public function actionSpool($methodName, $loopLimit = 1000, $chunkSize = 50)
+    {
+        if (!method_exists($this, $methodName)) {
+            throw new \InvalidArgumentException("Method {$methodName} not exist");
+        }
+
+        set_time_limit(0);
+        for ($i = 1; $i < $loopLimit; $i++) {
+            for ($j = 1; $j < $chunkSize; $j++) {
+                $r = call_user_func([$this, $methodName]);
+                if (!$r) {
+                    break 1;
+                }
+            }
+            sleep(1);
+        }
+    }
+}
