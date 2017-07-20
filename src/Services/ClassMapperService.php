f60f86d7282ba3abedf992c3c5389267c6db954f
Alexey <alexey.samoylov@gmail.com>
2017-07-20 19:13:25 +0700

    added class mapper service

---
+++ src/Services/ClassMapperService.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @author Alexey Samoylov <alexey.samoylov@gmail.com>
+ */
+
+namespace YarCode\Yii2\Services;
+
+class ClassMapperService
+{
+    /** @var string[] */
+    protected $classMap = [];
+
+    /**
+     * @return static
+     */
+    public static function getInstance()
+    {
+        /** @var static $obj */
+        $obj = \Yii::$container->get(self::class);
+        return $obj;
+    }
+
+    /**
+     * @param array $classMap
+     */
+    public function setClassMap($classMap)
+    {
+        $this->classMap = $classMap;
+    }
+
+    public function createObject($className)
+    {
+        $className = $this->resolve($className);
+        return new $className;
+    }
+
+    /**
+     * @param $className
+     * @return string
+     */
+    public function resolve($className)
+    {
+        if (!isset($this->classMap[$className])) {
+            return $className;
+        } else {
+            return $this->classMap[$className];
+        }
+    }
+}
